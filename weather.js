fec36cd561f6d38493fa0f29e9c73352b698fa2d
san00 <c5d59d07@opayq.com>
2017-09-21 14:24:39 +0100

    Get user location via IP address
    Removed geolocation and used another api to automatically get user
    coordinates.

--- weather.js
+++ weather.js
@@ -1,44 +1,40 @@
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(coords, error)
-} else {
-  alert("geolocation not available")
-}
+$.getJSON("https://ipapi.co/json/", function(data) {
+  var lat = data.latitude;
+  var long = data.longitude;
 
-function coords(position) {
-
-  var lat = position.coords.latitude;
-  var long = position.coords.longitude;
-
-  var apiInfo = "https://api.apixu.com/v1/current.json?key=043d3a0fb3e945d0a4d181246162911&q=" +
-    lat + "," + long;
-
+  var apiInfo =
+    "https://api.apixu.com/v1/current.json?key=043d3a0fb3e945d0a4d181246162911&q=" +
+    lat +
+    "," +
+    long;
 
   $.ajax({
     url: apiInfo,
     dataType: "json",
     success: function(weather) {
-
       $("#location").text(weather.location.region);
-      $('#temp_c').text(weather.current.temp_c);
-      $('#temp_f').text(weather.current.temp_f);
+      $("#temp_c").text(weather.current.temp_c);
+      $("#temp_f").text(weather.current.temp_f);
       $("#feelslike").text(weather.current.feelslike_c);
-      $("#weathericon").html('<img src="https:' + weather.current.condition.icon + '">');
+      $("#weathericon").html(
+        '<img src="https:' + weather.current.condition.icon + '">'
+      );
       $("#weathercondtext").text(weather.current.condition.text);
-      $('#time').text(weather.location.localtime);
+      $("#time").text(weather.location.localtime);
     }
   });
-}
 
-function error() {
-  alert('Sorry there was an error, try again');
-}
+  function error() {
+    alert("Sorry there was an error, try again");
+  }
 
-$("button").click(function() {
-  $("#temp_c").toggle();
-  $("#temp_f").toggle();
-});
+  $("button").click(function() {
+    $("#temp_c").toggle();
+    $("#temp_f").toggle();
+  });
 
-$("button").click(function() {
-  $("#text_c").toggle();
-  $("#text_f").toggle();
+  $("button").click(function() {
+    $("#text_c").toggle();
+    $("#text_f").toggle();
+  });
 });
